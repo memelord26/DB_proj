e9353b5b0f598d2accc491e44cdca4a33833aa7e
pheebs <116650062+memelord26@users.noreply.github.com>
2025-06-18 14:12:25 +0800

    blur background

--- app/page.js
+++ app/page.js
@@ -51,6 +51,7 @@ export default function Home() {
   return (
     <div>
       <div>
+        <div class="bg-image"></div>
         <nav className="bg-white border-gray-200 dark:bg-gray-700">
           <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
